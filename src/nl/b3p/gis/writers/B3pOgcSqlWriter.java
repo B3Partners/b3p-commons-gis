048ecf94a462ad1dd3fe4f255d0d44fdc1d37347
Roy Braam <braam.roy@gmail.com>
2007-10-29 12:36:22 +0000

    SQL writer verbeterd. Commentaar toegevoegd en wat code verplaatst.

--- src/nl/b3p/gis/writers/B3pOgcSqlWriter.java
+++ src/nl/b3p/gis/writers/B3pOgcSqlWriter.java
@@ -22,6 +22,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
@@ -42,16 +43,31 @@ public class B3pOgcSqlWriter {
     
     public static final String DEFAULT_GEOM_COLUMN="the_geom";
     public static final String PRODUCT_POSTGRES="PostgreSQL";
+    public static final String POSTGRES_GLOBALGEOMETRY="GEOMETRY";
+    public static final Set SUPPORTED_DIALECTS=new HashSet();   
+    
     /** Creates a new instance of B3pOgcSqlWriter */
     public B3pOgcSqlWriter(Connection conn){
-        setConnection(conn);        
+        SUPPORTED_DIALECTS.add(PRODUCT_POSTGRES);
+        setConnection(conn);  
     }
+    /* Creates a connection and a B3pOgcSqlWriter*/
     public B3pOgcSqlWriter(String url,String user,String password, java.sql.Driver driver) throws SQLException{
+        SUPPORTED_DIALECTS.add(PRODUCT_POSTGRES);
         DriverManager.registerDriver(driver);
         setConnection(DriverManager.getConnection(url,user,password));
-    }    
+    }
+    /* Write features to the given table.
+     * @param fc The collection of features.
+     * @param tablename The tablename.
+     * @param geomColumn The name of the geometry column. If null the attribute name of type geometry ikn the featureCollection is used.
+     * @param srid The srid of the features. If srid is null the srid of the geometry in the featurecollection is used. If that is also 0 or null -1 is used.
+     * @param dimension The dimension of the geometry in the feature. If 0 the dimension of the geometry is used. If still 0, 2 is used.
+     * @param createTabel If true the function will try to create a table if the given tablename doesn't exists. It only supports PostGres/Postgis for creating geometry columns!
+     * @param attributeNamesToLowerCase If true all attribute names will be cast to a lower case (recommended)
+     */
     public void write(FeatureCollection fc, String tablename, String geomColumn, String srid, int dimension, boolean createTable, boolean attributeNamesToLowerCase)throws SQLException, Exception{
-        //check if al features are of the same geom type
+        //check if al features are of the same geom type and get a geom
         Iterator fcit=fc.iterator();
         Geometry aGeom=null;
         boolean sameGeomType=true;
@@ -67,16 +83,16 @@ public class B3pOgcSqlWriter {
                 }
             }
         }
-        if(!sameGeomType)
-            throw new Exception("Not all features got the same Geometry Type");
-        //check if the table already exists
+        /*if(!sameGeomType)
+            throw new Exception("Not all features got the same Geometry Type");*/
         DatabaseMetaData dbmd = connection.getMetaData();
         List tableNames=OgcSqlUtils.getTableAndViewNames(connection);        
         FeatureSchema fs= fc.getFeatureSchema();         
         boolean wrongTable=false;
         if (tableNames!=null){
-            //if exists check the column's
+            //check if the table already exists
             if (tableNames.contains(tablename)){
+                //if exists check the column's
                 for (int i=0; i < fs.getAttributeCount() && !wrongTable; i++){
                     if (i!=fs.getGeometryIndex() || geomColumn==null){
                         boolean columFound=false;
@@ -85,13 +101,16 @@ public class B3pOgcSqlWriter {
                             attributeName=attributeName.toLowerCase();
                         }
                         AttributeType attributeType= fs.getAttributeType(i);
+                        //get the column by attribute name.
                         ResultSet rs = dbmd.getColumns(null, null, tablename, attributeName);
+                        //if the column exists check the compatibility of the data_type
                         if (rs.next()){
                             int type=rs.getInt("DATA_TYPE");
                             if(isCompatibleType(attributeType,type)){
                                 columFound=true;
                             }
                         }
+                        //if there is no correct columFound it is a 'wrong' table
                         if (!columFound){
                             wrongTable=true;
                         }
@@ -101,22 +120,25 @@ public class B3pOgcSqlWriter {
                             wrongTable=true;
                         }
                     }
-                }                
+                }
+                //if it is a wrong table (not compatible with featurecollection) return a error
                 if (wrongTable){
                     throw new Exception("Table already exists but has wrong columns.");
                 }
             }else{
-                //create table
+                //if the table doesn't exists and 'createTable'==true create the table
                 if (createTable){
-                    if (dbmd.getDatabaseProductName().equalsIgnoreCase(PRODUCT_POSTGRES)){                    
+                    //check if the product is supported
+                    if (SUPPORTED_DIALECTS.contains(dbmd.getDatabaseProductName())){                    
                         StringBuffer sb= new StringBuffer();
                         sb.append("CREATE TABLE ");
                         sb.append(tablename);
                         sb.append("(");
                         boolean firstColumn=true;
                         boolean geomColumnFound=false;
+                        //create a create table script without the geometry column.
                         for (int i=0; i < fs.getAttributeCount(); i++){
-                            AttributeType at=fs.getAttributeType(i);                        
+                            AttributeType at=fs.getAttributeType(i);                            
                             if (at.equals(AttributeType.GEOMETRY)){
                                 geomColumnFound=true;
                                 if (geomColumn==null){
@@ -127,7 +149,7 @@ public class B3pOgcSqlWriter {
                                         geomColumn=fs.getAttributeName(i);
                                     }
                                 }
-                            }else{
+                            }else{                                
                                 if (!firstColumn){
                                     sb.append(", ");
                                 }
@@ -151,44 +173,62 @@ public class B3pOgcSqlWriter {
                         }
                         sb.append(")"); 
                         StringBuffer sbg=null;
+                        //if there is a geomColum needed create the create script.
                         if(geomColumnFound){
-                            String s=dbmd.getURL();
-                            sbg=new StringBuffer();
-                            sbg.append("select addGeometryColumn(\'");
-                            String url=dbmd.getURL();
-                            String db=url.split("/")[url.split("/").length-1];                        
-                            sbg.append(db);
-                            sbg.append("\', \'");
-                            sbg.append(tablename);
-                            sbg.append("\', \'");
-                            sbg.append(geomColumn);
-                            sbg.append("\', ");
-                            if (srid!=null){
-                                sbg.append(srid);
-                            }else{
-                                if (aGeom.getSRID()>0){
-                                    sbg.append(fs.getCoordinateSystem().getEPSGCode());
+                            //if it is a postgres/postgis db
+                            if (dbmd.getDatabaseProductName().equalsIgnoreCase(PRODUCT_POSTGRES)){
+                                // get the database name.
+                                String s=dbmd.getURL();
+                                String url=dbmd.getURL();
+                                String db=url.split("/")[url.split("/").length-1];
+                                //create the script for the addGeometryColumn function
+                                sbg=new StringBuffer();
+                                sbg.append("select addGeometryColumn(\'");                                
+                                sbg.append(db);
+                                sbg.append("\', \'");
+                                sbg.append(tablename);
+                                sbg.append("\', \'");
+                                sbg.append(geomColumn);
+                                sbg.append("\', ");
+                                //if the srid is given as param use it. Otherwise try to get the srid out of the geometry or use -1 as srid
+                                if (srid!=null){
+                                    sbg.append(srid);
                                 }else{
-                                    sbg.append(-1);
+                                    if (aGeom.getSRID()>0){
+                                        sbg.append(fs.getCoordinateSystem().getEPSGCode());
+                                    }else{
+                                        sbg.append(-1);
+                                    }
                                 }
-                            }
-                            sbg.append(", \'");
-                            sbg.append(aGeom.getGeometryType().toUpperCase());
-                            sbg.append("\', ");
-                            if (dimension>0){
-                                sbg.append(dimension);
-                            }else{
-                                if (aGeom.getDimension()>1)
-                                    sbg.append(aGeom.getDimension());
-                                else{
-                                    sbg.append("2");
+                                sbg.append(", \'");
+                                //if not all geometries are of the same type create a global geometry column
+                                if (!sameGeomType){
+                                    sbg.append(POSTGRES_GLOBALGEOMETRY);
+                                }else{
+                                    sbg.append(aGeom.getGeometryType().toUpperCase());
+                                }                                    
+                                sbg.append("\', ");
+                                //if the dimension is given use it, otherwise try to get it out of the Geometry of use 2
+                                if (dimension>0){
+                                    sbg.append(dimension);
+                                }else{
+                                    if (aGeom.getDimension()>1)
+                                        sbg.append(aGeom.getDimension());
+                                    else{
+                                        sbg.append("2");
+                                    }
                                 }
+                                sbg.append(")");
                             }
-                            sbg.append(")");                        
                         }
+                        //execute the create table script.
                         PreparedStatement statement = null;
                         try{
-                            statement=connection.prepareStatement(sb.toString()+";"+sbg.toString());
+                            if (sbg.toString()!=null){
+                                sb.append(";");
+                                sb.append(sbg.toString());
+                            }
+                            statement=connection.prepareStatement(sb.toString());
                             statement.execute();                        
                         }catch(SQLException se){
                             log.error("Query: "+sb.toString(),se);
@@ -201,14 +241,14 @@ public class B3pOgcSqlWriter {
                         throw new Exception("CREATE TABLE not supported for "+dbmd.getDatabaseProductName());
                     }
                 }else{
-                    throw new Exception("Table doesn't exists");
+                    throw new Exception("Table does not exists");
                 }
-            }
+            }           
             StringBuffer insertPart = new StringBuffer();
             insertPart.append("INSERT INTO ");
             insertPart.append(tablename);
             insertPart.append(" (");
-           
+            //create the global part of the insert script.
             for (int i=0; i < fs.getAttributeCount(); i++){
                 if (i!=0)
                     insertPart.append(", ");
@@ -224,10 +264,12 @@ public class B3pOgcSqlWriter {
             }
             insertPart.append(") VALUES ");
             Iterator it= fc.iterator();
+            //Create the insert script.
+            StringBuffer q = null;
             while (it.hasNext()){
-                Feature f= (Feature)it.next();         
-                
-                StringBuffer q = new StringBuffer();
+                if (q==null)
+                    q=new StringBuffer();
+                Feature f= (Feature)it.next();
                 q.append(insertPart.toString());
                 q.append("(");
                 for (int i=0; i < fs.getAttributeCount(); i++){
@@ -250,12 +292,15 @@ public class B3pOgcSqlWriter {
                         }
                     }
                 }
-                q.append(")");
+                q.append(");");                
+            }
+            //excecute the insert script if there is data to add.
+            if (q!=null){
                 PreparedStatement statement=null;
-                try{
+                try{                
                     statement=connection.prepareStatement(q.toString());
                     statement.execute();
-                    
+
                 }catch(SQLException se){
                     log.error("Can't execute query: "+q.toString(), se);
                     throw se;
@@ -263,7 +308,8 @@ public class B3pOgcSqlWriter {
                     if (statement!=null)
                         statement.close();
                 }
-            }                
+            }
+            
         }
     }    
     public Connection getConnection() {
@@ -272,6 +318,12 @@ public class B3pOgcSqlWriter {
     public void setConnection(Connection connection) {
         this.connection = connection;
     }
+    /**
+     * Check if the given attributetype is compatible with the given sql type
+     * @param attributeType the attributeType of the feature attribute
+     * @param type the sql type of the column.
+     * @return true if compatible
+     */
     private boolean isCompatibleType(AttributeType attributeType, int type) {
         if (attributeType.equals(AttributeType.DATE)){         
             if (type==java.sql.Types.DATE || type==java.sql.Types.TIME || type==java.sql.Types.TIMESTAMP)
@@ -297,7 +349,7 @@ public class B3pOgcSqlWriter {
     
      public static void main(String [] args) throws IOException, ParseException, Exception{
          String w1url="http://w1.b3p.nl/cgi-bin/mapserv.exe?SRSNAME=EPSG:28992&TYPENAME=tankstations_centroid&BBOX=70000,300000,305000,425000&VERSION=1.0.0&SERVICE=WFS&map=e:/mapserver/pnb_wis/pnb_wis2.map&REQUEST=GetFeature";
-         String royurl="http://b3p-roy/cgi-bin/mapserv.exe?map=C:/mapserver/map/pnb_wis/geoplaza.map&SERVICE=WFS&REQUEST=GetFeature&VERSION=1.0.0&TYPENAME=waterschapsgrenzen&BBOX=70000,300000,305000,425000&SRSNAME=EPSG:28992";         
+         String royurl="http://b3p-roy/cgi-bin/mapserv.exe?map=C:/mapserver/map/pnb_wis/geoplaza.map&SERVICE=WFS&REQUEST=GetFeature&VERSION=1.0.0&TYPENAME=strooiroutes&BBOX=70000,300000,305000,425000&SRSNAME=EPSG:28992";         
          String url="jdbc:postgresql://localhost:5432/pnb_wis";
          String user="postgres";
          String password="***REMOVED***";
