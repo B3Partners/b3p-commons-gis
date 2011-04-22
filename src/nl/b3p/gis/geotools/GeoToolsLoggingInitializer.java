ae1f562833a4d15d2ff0828cb48ddc570aca5f6c
Matthijs Laan <matthijsln@gmail.com>
2011-04-22 11:37:30 +0000

    verplaatst naar b3p-commons-gis vanaf b3p-commons-core

---
+++ src/nl/b3p/gis/geotools/GeoToolsLoggingInitializer.java
@@ -0,0 +1,49 @@
+/**
+ * $Id$
+ */
+
+package nl.b3p.commons.geotools;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.geotools.util.logging.Logging;
+
+/**
+ * Configureer in web.xml als:
+    <listener>
+        <listener-class>nl.b3p.commons.geotools.GeoToolsLoggingInitializer</listener-class>
+    </listener>
+ *
+ * Voorbeeld log4j.properties:
+log4j.logger.org.geotools=WARN
+log4j.logger.org.geotools.data.wfs=ERROR
+log4j.logger.org.geotools.factory=ERROR
+ */
+public class GeoToolsLoggingInitializer implements ServletContextListener {
+
+    public void contextInitialized(ServletContextEvent sce) {
+        /* Van http://docs.codehaus.org/display/GEOTDOC/Logging */
+
+        final Logging logging = Logging.ALL;
+        try {
+            logging.setLoggerFactory("org.geotools.util.logging.CommonsLoggerFactory");
+        } catch (ClassNotFoundException commonsException) {
+            try {
+                logging.setLoggerFactory("org.geotools.util.logging.Log4JLoggerFactory");
+            } catch (ClassNotFoundException log4jException) {
+                // Nothing to do, we already tried our best.
+            }
+        }
+
+        /* De INFO messages van deze logger zijn niet erg boeiend en zorgen
+         * voor een boel logregels.
+         */
+        Logger l = Logger.getRootLogger().getLoggerRepository().getLogger("org.geotools.factory");
+        l.setLevel(Level.WARN);
+    }
+
+    public void contextDestroyed(ServletContextEvent sce) {
+    }
+}
