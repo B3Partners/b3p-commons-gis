296bb4a1ee368ebe922e3021a64cbb5a88b4669a
Jytte Schaeffer <jytteschaeffer@b3partners.nl>
2013-04-04 09:27:35 +0000

    correct paths

--- xsd/createObjects.bat
+++ xsd/createObjects.bat
@@ -15,26 +15,26 @@ echo %CP%
 @rem Java 2 style collection types
 
 @echo GML
-"%JDK_DIR%\bin\java" -cp %CP% org.exolab.castor.builder.SourceGenerator -i v110/customGML.xsd -f -types j2 -package nl.b3p.xml.gml.v311
+"%JDK_DIR%\bin\java" -cp %CP% org.exolab.castor.builder.SourceGenerator -i WFS110/v110/customGML.xsd -f -types j2 -package nl.b3p.xml.gml.v311
 @echo OGC
-"%JDK_DIR%\bin\java" -cp %CP% org.exolab.castor.builder.SourceGenerator -i v110/expr.xsd -f -types j2 -package nl.b3p.xml.ogc.v110
-"%JDK_DIR%\bin\java" -cp %CP% org.exolab.castor.builder.SourceGenerator -i v110/filter.xsd -f -types j2 -package nl.b3p.xml.ogc.v110
-"%JDK_DIR%\bin\java" -cp %CP% org.exolab.castor.builder.SourceGenerator -i v110/filterCapabilities.xsd -f -types j2 -package nl.b3p.xml.ogc.v110
-"%JDK_DIR%\bin\java" -cp %CP% org.exolab.castor.builder.SourceGenerator -i v110/sort.xsd -f -types j2 -package nl.b3p.xml.ogc.v110
+"%JDK_DIR%\bin\java" -cp %CP% org.exolab.castor.builder.SourceGenerator -i WFS110/v110/expr.xsd -f -types j2 -package nl.b3p.xml.ogc.v110
+"%JDK_DIR%\bin\java" -cp %CP% org.exolab.castor.builder.SourceGenerator -i WFS110/v110/filter.xsd -f -types j2 -package nl.b3p.xml.ogc.v110
+"%JDK_DIR%\bin\java" -cp %CP% org.exolab.castor.builder.SourceGenerator -i WFS110/v110/filterCapabilities.xsd -f -types j2 -package nl.b3p.xml.ogc.v110
+"%JDK_DIR%\bin\java" -cp %CP% org.exolab.castor.builder.SourceGenerator -i WFS110/v110/sort.xsd -f -types j2 -package nl.b3p.xml.ogc.v110
 @echo OWS
-"%JDK_DIR%\bin\java" -cp %CP% org.exolab.castor.builder.SourceGenerator -i v110/owsAll.xsd -f -types j2 -package nl.b3p.xml.ows.v100
-"%JDK_DIR%\bin\java" -cp %CP% org.exolab.castor.builder.SourceGenerator -i v110/ows19115subset.xsd -f -types j2 -package nl.b3p.xml.ows.v100
-"%JDK_DIR%\bin\java" -cp %CP% org.exolab.castor.builder.SourceGenerator -i v110/owsCommon.xsd -f -types j2 -package nl.b3p.xml.ows.v100
-"%JDK_DIR%\bin\java" -cp %CP% org.exolab.castor.builder.SourceGenerator -i v110/owsDataIdentification.xsd -f -types j2 -package nl.b3p.xml.ows.v100
-"%JDK_DIR%\bin\java" -cp %CP% org.exolab.castor.builder.SourceGenerator -i v110/owsExceptionReport.xsd -f -types j2 -package nl.b3p.xml.ows.v100
-"%JDK_DIR%\bin\java" -cp %CP% org.exolab.castor.builder.SourceGenerator -i v110/owsGetCapabilities.xsd -f -types j2 -package nl.b3p.xml.ows.v100
-"%JDK_DIR%\bin\java" -cp %CP% org.exolab.castor.builder.SourceGenerator -i v110/owsOperationsMetadata.xsd -f -types j2 -package nl.b3p.xml.ows.v100
-"%JDK_DIR%\bin\java" -cp %CP% org.exolab.castor.builder.SourceGenerator -i v110/owsServiceIdentification.xsd -f -types j2 -package nl.b3p.xml.ows.v100
-"%JDK_DIR%\bin\java" -cp %CP% org.exolab.castor.builder.SourceGenerator -i v110/owsServiceProvider.xsd -f -types j2 -package nl.b3p.xml.ows.v100
+"%JDK_DIR%\bin\java" -cp %CP% org.exolab.castor.builder.SourceGenerator -i WFS110/v110/owsAll.xsd -f -types j2 -package nl.b3p.xml.ows.v100
+"%JDK_DIR%\bin\java" -cp %CP% org.exolab.castor.builder.SourceGenerator -i WFS110/v110/ows19115subset.xsd -f -types j2 -package nl.b3p.xml.ows.v100
+"%JDK_DIR%\bin\java" -cp %CP% org.exolab.castor.builder.SourceGenerator -i WFS110/v110/owsCommon.xsd -f -types j2 -package nl.b3p.xml.ows.v100
+"%JDK_DIR%\bin\java" -cp %CP% org.exolab.castor.builder.SourceGenerator -i WFS110/v110/owsDataIdentification.xsd -f -types j2 -package nl.b3p.xml.ows.v100
+"%JDK_DIR%\bin\java" -cp %CP% org.exolab.castor.builder.SourceGenerator -i WFS110/v110/owsExceptionReport.xsd -f -types j2 -package nl.b3p.xml.ows.v100
+"%JDK_DIR%\bin\java" -cp %CP% org.exolab.castor.builder.SourceGenerator -i WFS110/v110/owsGetCapabilities.xsd -f -types j2 -package nl.b3p.xml.ows.v100
+"%JDK_DIR%\bin\java" -cp %CP% org.exolab.castor.builder.SourceGenerator -i WFS110/v110/owsOperationsMetadata.xsd -f -types j2 -package nl.b3p.xml.ows.v100
+"%JDK_DIR%\bin\java" -cp %CP% org.exolab.castor.builder.SourceGenerator -i WFS110/v110/owsServiceIdentification.xsd -f -types j2 -package nl.b3p.xml.ows.v100
+"%JDK_DIR%\bin\java" -cp %CP% org.exolab.castor.builder.SourceGenerator -i WFS110/v110/owsServiceProvider.xsd -f -types j2 -package nl.b3p.xml.ows.v100
 @echo XLINK
-"%JDK_DIR%\bin\java" -cp %CP% org.exolab.castor.builder.SourceGenerator -i v110/xlinks.xsd -f -types j2 -package nl.b3p.xml.xlink
+"%JDK_DIR%\bin\java" -cp %CP% org.exolab.castor.builder.SourceGenerator -i WFS110/v110/xlinks.xsd -f -types j2 -package nl.b3p.xml.xlink
 @echo WFS
-"%JDK_DIR%\bin\java" -cp %CP% org.exolab.castor.builder.SourceGenerator -i v110/wfs.xsd -f -types j2 -package nl.b3p.xml.wfs.v110
+"%JDK_DIR%\bin\java" -cp %CP% org.exolab.castor.builder.SourceGenerator -i WFS110/v110/wfs.xsd -f -types j2 -package nl.b3p.xml.wfs.v110
 
 @echo.
 @echo About to compile generated source code... Check for failures
