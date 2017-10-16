9e9a9fead8f45f6893cf7f70340d5af34c9ff6a6
Mark Prins <markprins@b3partners.nl>
2017-10-16 12:34:02 +0200

    fixup geotools logging redirect

--- src/main/java/nl/b3p/gis/geotools/GeoToolsLoggingInitializer.java
+++ src/main/java/nl/b3p/gis/geotools/GeoToolsLoggingInitializer.java
@@ -1,39 +1,39 @@
 /**
  * $Id$
  */
-
 package nl.b3p.gis.geotools;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import org.geotools.factory.GeoTools;
 import org.geotools.util.logging.Logging;
 
 /**
- * Configureer in web.xml als:
-    <listener>
-        <listener-class>nl.b3p.gis.geotools.GeoToolsLoggingInitializer</listener-class>
-    </listener>
- *
- * Voorbeeld log4j.properties:
-log4j.logger.org.geotools=WARN
-log4j.logger.org.geotools.data.wfs=ERROR
-log4j.logger.org.geotools.factory=ERROR
+ * Configureer in web.xml als: <br> {@code
+ * <listener>
+ * <listener-class>nl.b3p.gis.geotools.GeoToolsLoggingInitializer</listener-class>
+ * </listener>
+ * }
+ * <br>
+ * Voorbeeld log4j.properties: <br> {@code
+ * log4j.logger.org.geotools=WARN
+ * log4j.logger.org.geotools.data.wfs=ERROR
+ * log4j.logger.org.geotools.factory=ERROR
+ * }
  */
-public class GeoToolsLoggingInitializer implements ServletContextListener{
-    private static final Log log = LogFactory.getLog(GeoToolsLoggingInitializer.class);
+public class GeoToolsLoggingInitializer implements ServletContextListener {
+
     public void contextInitialized(ServletContextEvent sce) {
-        log.info("Setting up log4j fot geotools");
-        try{
+        try {
+            GeoTools.init();
             Logging.ALL.setLoggerFactory("org.geotools.util.logging.Log4JLoggerFactory");
-        }catch (Exception e){
-            log.error("While setting log4j for geotools",e);
+        } catch (ClassNotFoundException | IllegalArgumentException e) {
+            sce.getServletContext().log(e, "Omzetten GeoTools logger is mislukt.");
         }
     }
 
     public void contextDestroyed(ServletContextEvent sce) {
-        
+        org.geotools.util.WeakCollectionCleaner.DEFAULT.exit();
     }
-    
+
 }
